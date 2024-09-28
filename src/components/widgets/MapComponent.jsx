d995a8c3fec7371387ffe6117dadf5e85b975c78
kieefman <cristobal.gonzalez04@alumnos.uach.cl>
2024-09-28 15:38:40 -0300

    Leaflet Icon Fix

--- src/components/widgets/MapComponent.jsx
+++ src/components/widgets/MapComponent.jsx
@@ -2,6 +2,15 @@ import React, { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+// Configura la ruta de los íconos de Leaflet
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+});
+
 const LeafletMap = ({ lat, lon, zoom, markers }) => {
   useEffect(() => {
     // Solo ejecuta este código en el cliente
