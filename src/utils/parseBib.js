150b68b27c33eae0729c4f683bb1b5b1afd83d05
kieefman <cristobal.gonzalez04@alumnos.uach.cl>
2024-11-03 09:02:53 -0300

    Update to review

---
+++ src/utils/parseBib.js
@@ -0,0 +1,12 @@
+// src/utils/parseBib.js
+import fs from 'fs';
+import bibtexParse from 'bibtex-parser-js';
+
+export function parseBibFile(filePath) {
+  const bibContent = fs.readFileSync(filePath, 'utf8');
+  const parsedData = bibtexParse.toJSON(bibContent);
+  
+  console.log("Parsed BibTeX Data:", parsedData); // Agrega este log para revisar el contenido
+
+  return parsedData;
+}
