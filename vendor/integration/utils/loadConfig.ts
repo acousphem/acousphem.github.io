156c1e367418998b269d09cb4898142aea4a91b2
kieefman <cristobal.gonzalez04@alumnos.uach.cl>
2025-04-06 13:47:55 -0400

    Delete some old pages & googlesiteid test1

--- vendor/integration/utils/loadConfig.ts
+++ vendor/integration/utils/loadConfig.ts
@@ -1,7 +1,25 @@
 import fs from 'node:fs';
 import yaml from 'js-yaml';
 
+const replaceEnvVariables = (config: any): any => {
+  if (typeof config === 'string') {
+    // Reemplaza variables de entorno en cadenas
+    return config.replace(/\$\{(\w+)\}/g, (_, envVar) => process.env[envVar] || '');
+  } else if (Array.isArray(config)) {
+    // Reemplaza variables de entorno en arrays
+    return config.map(replaceEnvVariables);
+  } else if (typeof config === 'object' && config !== null) {
+    // Reemplaza variables de entorno en objetos
+    for (const key in config) {
+      config[key] = replaceEnvVariables(config[key]);
+    }
+  }
+  return config;
+};
+
 const loadConfig = async (configPathOrData: string | object) => {
+  let config;
+  
   if (typeof configPathOrData === 'string') {
     const content = fs.readFileSync(configPathOrData, 'utf8');
     if (configPathOrData.endsWith('.yaml') || configPathOrData.endsWith('.yml')) {
@@ -10,7 +28,8 @@ const loadConfig = async (configPathOrData: string | object) => {
     return content;
   }
 
-  return configPathOrData;
+  // Reemplaza las variables de entorno en el contenido cargado
+  return replaceEnvVariables(config);
 };
 
 export default loadConfig;
